0faefa502408bbcc0390f9cb95c02febb7476c56
jatinbusa6-tech <jatinbusa6@gmail.com>
2026-01-10 12:02:36 +0530

    Add files via upload

---
+++ day2/2.py
@@ -0,0 +1,4 @@
+name="abc"
+std=2
+
+print(f"Hello, My name is {name}, I'M IN {std} std.")
---
+++ day2/3.py
@@ -0,0 +1,20 @@
+""""
+File:
+Project:
+Author:
+Version:
+Release Date:
+"""
+
+#string line comment
+
+print("test")
+
+# doc string (__doc__)
+""""
+this is 
+multi
+line comment
+"""
+
+print("test")
