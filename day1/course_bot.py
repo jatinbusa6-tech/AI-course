e393750265b2ecd0036ccadfe1fc901a7f903e6a
jatinbusa6-tech <jatinbusa6@gmail.com>
2026-01-10 06:50:42 +0000

    text

---
+++ day1/course_bot.py
@@ -0,0 +1,7 @@
+question =input("ask any bquestion related to course:")
+
+if question == "what is thish course about":
+    print("thish course i8s about learning practical appliction raleted to ai")
+
+elif question == "duration":
+    print("this is 30 hours course")
