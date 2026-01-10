9092c2b7699a713e70da069e2afa40edad45d479
jatinbusa6-tech <jatinbusa6@gmail.com>
2026-01-10 12:02:10 +0530

    Add files via upload

---
+++ day1/cyber.py.py
@@ -0,0 +1,47 @@
+question = input("aska question about cyber security:")
+#h1
+if question =="what is cyber security ?":
+    print("Cybersecurity is the practice of protecting computer systems, networks, and data from digital attacks:")
+
+#h2
+elif question == "Why is cybersecurity important?":
+    print("It helps protect personal information, financial data, and systems from hackers and cybercrime.")
+
+#h3
+elif question =="What is malware?":
+    print("Malware is malicious software designed to damage, steal, or disrupt computer systems.")
+#h4
+elif question =="What is a virus in cybersecurity?":
+    print("A virus is a type of malware that attaches itself to files and spreads when the file is opened.")
+
+#h5
+elif question =="What is phishing?":
+    print("Phishing is a cyber attack where fake emails or messages trick users into sharing sensitive information.")
+
+#h6
+elif question =="What is a firewall?":
+    print("A firewall is a security system that monitors and controls incoming and outgoing network traffic.")
+
+#h7
+elif question =="What is encryption?":
+    print("Encryption is the process of converting data into a secure code to prevent unauthorized access.")
+
+#h8
+elif question =="What is a strong password?":
+    print("A strong password includes letters, numbers, and symbols and is hard to guess.")
+
+#h9
+elif question =="What is two-factor authentication (2FA)?":
+    print("2FA adds an extra security step by requiring a second form of verification.")
+
+#h10
+elif question =="What is a data breach?":
+    print("A data breach occurs when confidential information is accessed without permission.")
+
+#h11
+elif question =="What is ransomware?":
+    print("Ransomware is malware that locks data and demands payment to unlock it.")  
+
+#h12
+elif question =="What is social engineering?":
+    print("Social engineering is tricking people into revealing confidential information.")
