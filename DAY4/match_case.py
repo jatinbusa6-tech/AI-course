f3994a2ecb208ac86485eb9647a5b1ddd407d909
jatinbusa6-tech <jatinbusa6@gmail.com>
2026-02-22 16:27:24 +0530

    Add files via upload

---
+++ DAY4/match_case.py
@@ -0,0 +1,13 @@
+
+# match...case (python version >=3.10)
+
+day = int(input("enter day:"))
+
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case _:
+        # thi is default case
+        print("bas sunday sudi j hoi")  
